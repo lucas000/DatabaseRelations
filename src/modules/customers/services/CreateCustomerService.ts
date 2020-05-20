156ac3d4d04b23bff5eefde568cb2a08c6210bc2
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2020-05-20 12:05:49 -0300

    Create new product

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,23 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const checkCustomerExists = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (checkCustomerExists) {
+      throw new AppError('Customer already exists');
+    }
+
+    const user = await this.customersRepository.create({ name, email });
+
+    return user;
   }
 }
 
